d81f71ad8f7016096db8cd21111402c14f348920
Joel Peckham <joelskyler@gmail.com>
2022-04-25 17:15:58 -0400

    article title

--- infer.py
+++ infer.py
@@ -100,7 +100,7 @@ if __name__ == "__main__":
         for article in testFiles[:limit]:
             context = article['context']
             if len(context.split()) > 1010:
-                print(f"Skipping {article['url']} because it is too long")
+                print(f"Skipping {article['title']} because it is too long")
                 continue
             tokens = tokenizer.encode(context)
             start = time.time()
